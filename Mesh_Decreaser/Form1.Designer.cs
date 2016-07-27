59a146658fae9fec3b59d8f527d0fd7919fc19cd
Maeda Hiromu <hiro1616be@gmail.com>
2016-07-27 15:58:24 +0900

    20160727 とりあえずファイル読み込むとこまで

--- Mesh_Decreaser/Form1.Designer.cs
+++ Mesh_Decreaser/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -54,11 +55,19 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(502, 19);
+            this.textBox2.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(527, 467);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -73,6 +82,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Mesh_Decreaser/Form1.cs
+++ Mesh_Decreaser/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace Mesh_Decreaser
 {
@@ -19,6 +21,37 @@ namespace Mesh_Decreaser
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            string fn = openFileDialog1.FileName;
+            textBox1.Text = fn;
+            if (File.Exists(fn))
+            {
+                XmlTextReader reader = null;
+                try
+                {
+                    reader = new XmlTextReader(fn);
+                    //ストリームからノードを読み取る
+                    {
+                        if (reader.NodeType == XmlNodeType.Element)
+                        {
+                            switch (reader.LocalName)
+                            {
+                                case "gml:high":
+                                    textBox2.Text += reader.ReadString() + "\r\n";
+                                    break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
 
         }
     }
